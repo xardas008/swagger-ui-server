70da33255dc00f8e535bee6064283c1e18cbb8b7
Daniel Kreuter <daniel.kreuter@1und1.de>
2017-07-14 09:13:06 +0200

    Initial version.

---
+++ src/main/java/com/xardas008/swagger/swaggeruiserver/SwaggerUiServerApplication.java
@@ -0,0 +1,12 @@
+package com.xardas008.swagger.swaggeruiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerUiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerUiServerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/xardas008/swagger/swaggeruiserver/SwaggerUiServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xardas008.swagger.swaggeruiserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SwaggerUiServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
